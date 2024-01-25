3607c8ae618c16dfcadadfdfb52f122258700a98
krizhnaa <krizhnaprem@gmail.com>
2024-01-25 11:40:43 +0530

    Created a dictionary with element_id and its value

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 chrome_driver_path = ChromeDriverManager().install()
 chrome_options = webdriver.ChromeOptions()
@@ -17,13 +18,28 @@ cookie.click()
 
 our_money_str = driver.find_element(By.CSS_SELECTOR, value="#money")
 our_money = int(our_money_str.text)
-print(our_money)
+# print(our_money)
 
-store_monies = driver.find_elements(By.CSS_SELECTOR, value="#store b")
-for money in store_monies[:len(store_monies)-1]:
-    parts = money.text.split('-')
-    numeric_value = parts[1].strip()
-    numeric_value = int(numeric_value.replace(',', ''))
-    print(numeric_value)
+store_dict = {}
+
+store_monies = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+for item in store_monies[:len(store_monies)-1]:
+    item_id = item.get_attribute("id")
+    # print(item.text)
+    parts1 = item.text.split('-')
+    parts2 = parts1[1].split('\n')
+    store_money = int(parts2[0].replace(',', ''))
+    print(store_money)
+
+    store_dict[item] = {
+        item_id: store_money
+    }
+
+    # if our_money >= store_money:
+    #
+    #
+    # time.sleep(5)
+
+print(store_dict)
 
 driver.quit()
