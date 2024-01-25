e3af9c2e15ec009f2e7c99e173e0b4068036f605
krizhnaa <krizhnaprem@gmail.com>
2024-01-25 10:49:02 +0530

    Adding the site and selecting a few elements

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ chrome_driver_path = ChromeDriverManager().install()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option(name="detach", value=True)
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+
+URL = "https://orteil.dashnet.org/experiments/cookie/"
+driver.get(url=URL)
+
+# while True:
+cookie = driver.find_element(By.ID, value="cookie")
+cookie.click()
+
+our_money = driver.find_element(By.CSS_SELECTOR, value="#money")
+print(our_money.text)
+
+store_monies = driver.find_elements(By.CSS_SELECTOR, value="#store")
+
+
+driver.quit()
